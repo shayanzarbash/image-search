cce26f10bfd9027eb3ad9d00cfff49022f4e82de
shayanzarbash <shayan.zarbash@gmail.com>
2023-04-16 16:51:28 +0430

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [loca, setLoca] = useState({});
+  const [weather, setWeather] = useState([]);
+
+  const getLocation = async () => {
+    const urlIp = 'http://ip-api.com/json/?fields=status,message,country,city,lat,lon,timezone';
+    const response = await fetch(urlIp).then((response) => response.json());
+    setLoca(response);
+    console.log(response);
+    return response;
+  }
+
+  const getWeather = async (lat, lon) => {
+    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f0894defae7c5584798f8812232a40c2`;
+    const response = await fetch(urlWeather).then((response) => response.json());
+    setWeather(response);
+    console.log(response);
+    return response;
+  }
+
+  useEffect(() => {
+    getLocation().then((data) => {
+      console.log("data", data);
+      getWeather(data.lat, data.lon);
+    });
+  }, []);
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  console.log("loca", loca);
+  console.log("weather", weather.dt);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {loca && loca.country}
+      </div>
+
+    </>
   );
 }
 
